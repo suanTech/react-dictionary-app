46d558f6255215c3f87406b0ca6634653ac2c89d
Suan Choi <siasiarhdwn@gmail.com>
2023-02-11 10:56:31 +0800

    synonym contains more words

--- src/components/result/Synonym.js
+++ src/components/result/Synonym.js
@@ -1,23 +1,23 @@
-import { SynonymContainer, SynonymWrapper } from "../../styles/result"
-import { HeadingTextS } from "../../styles/text"
+import { SynonymContainer, SynonymWrapper } from "../../styles/result";
+import { HeadingTextS } from "../../styles/text";
 
-export const Synonym = ({synonyms}) => {
+export const Synonym = ({ synonyms }) => {
   return (
     <>
-    { synonyms.length ? (
-      <SynonymWrapper>
-        <HeadingTextS className="muted">Synonyms</HeadingTextS>
-        <SynonymContainer>
-          {synonyms.map((synonym, i) => (
-            <HeadingTextS key={i} className="accent">
-              {synonym}
-            </HeadingTextS>
-          ))}
-        </SynonymContainer>
-      </SynonymWrapper>
-    ) : (
-      ""
-    )}
+      {synonyms.length ? (
+        <SynonymWrapper>
+          <HeadingTextS className="muted">Synonyms</HeadingTextS>
+          <SynonymContainer>
+            {synonyms.map((synonym, i) => (
+              <HeadingTextS key={i} className="accent">
+                {synonym}
+              </HeadingTextS>
+            ))}
+          </SynonymContainer>
+        </SynonymWrapper>
+      ) : (
+        ""
+      )}
     </>
-  )
-}
+  );
+};
